316dae766792ffa003812345eccdc12516568dc5
Ali.Dev <aliry@live.com>
2024-04-05 00:57:38 -0700

    add useSocketRef

--- client/src/gameState/gameHooks.ts
+++ client/src/gameState/gameHooks.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { useAtom } from 'jotai';
 import { errorAtom, gameStateAtom } from './gameState';
+import { Socket, io } from 'socket.io-client';
+import { SocketEvents } from '../constants';
 const serverURL = 'http://localhost:3001';
 
 export const useCreateGame = (playerName: string) => {
@@ -23,4 +25,31 @@ export const useCreateGame = (playerName: string) => {
   }, [playerName, setError, setGameState]);
 
   return handleCreateGame;
+};
+
+export const useSocketRef = () => {
+  const socketRef = useRef<Socket | null>(null);
+
+  useEffect(() => {
+    if (!socketRef.current) {
+      socketRef.current = io(serverURL, {
+        transports: ['websocket']
+      });
+
+      socketRef.current.on('connect_error', (err: { message: any }) => {
+        console.log(`connect_error due to ${err.message}`);
+      });
+
+      return () => {
+        if (socketRef.current) {
+          socketRef.current.off('connect_error');
+          socketRef.current.off(SocketEvents.ServerEvent);
+          socketRef.current.disconnect();
+          socketRef.current = null;
+        }
+      };
+    }
+  }, []);
+
+  return socketRef.current;
 };
