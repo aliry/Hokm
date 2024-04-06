3ccf94764518ebd59f2a06adf8c98a344d518622
Ali.Dev <aliry@live.com>
2024-04-05 19:46:28 -0700

    add more hooks

--- client/src/gameState/gameHooks.ts
+++ client/src/gameState/gameHooks.ts
@@ -1,13 +1,19 @@
 import axios from 'axios';
 import { useCallback, useEffect, useRef } from 'react';
 import { useAtom } from 'jotai';
-import { errorAtom, gameStateAtom } from './gameState';
+import {
+  cardsAtom,
+  errorAtom,
+  gameInitStateAtom,
+  gameStateAtom
+} from './gameState';
 import { Socket, io } from 'socket.io-client';
-import { SocketEvents } from '../constants';
+import { GameAction, GameEvent, SocketEvents } from '../constants';
+import { ServerEventPayload } from '../sharedTypes';
 const serverURL = 'http://localhost:3001';
 
 export const useCreateGame = (playerName: string) => {
-  const [, setGameState] = useAtom(gameStateAtom);
+  const [, setGameState] = useAtom(gameInitStateAtom);
   const [, setError] = useAtom(errorAtom);
   const handleCreateGame = useCallback(() => {
     axios
@@ -52,4 +58,73 @@ export const useSocketRef = () => {
   }, []);
 
   return socketRef.current;
+};
+
+export const useEmitAction = (socketRef: Socket | null) => {
+  const emitAction = useCallback(
+    (action: string, data: any) => {
+      if (!socketRef) {
+        return;
+      }
+      const payload = { action, data };
+      socketRef.emit(SocketEvents.ClientAction, payload);
+    },
+    [socketRef]
+  );
+
+  return emitAction;
+};
+
+export const useJoinGame = (socketRef: Socket | null) => {
+  const [, setGameState] = useAtom(gameInitStateAtom);
+  const emitAction = useEmitAction(socketRef);
+  const joinGame = useCallback(
+    (teamCode: string, playerName: string) => {
+      if (!socketRef || !teamCode || !playerName) {
+        return;
+      }
+
+      setGameState((prev) => ({ ...prev, teamCode }));
+      emitAction(GameAction.JoinGame, { teamCode, playerName });
+    },
+    [emitAction, setGameState, socketRef]
+  );
+
+  return joinGame;
+};
+
+export const useSocketEvents = (socket: Socket) => {
+  const [, setErrors] = useAtom(errorAtom);
+  const [, setCards] = useAtom(cardsAtom);
+  const [, setGameState] = useAtom(gameStateAtom);
+
+  const handleSocketEvents = useCallback(() => {
+    socket.on(SocketEvents.ServerEvent, (payload: ServerEventPayload) => {
+      console.log(payload);
+      if (payload.event === GameEvent.Error) {
+        setErrors(payload.data);
+      } else {
+        if (payload.gameState) {
+          const cards = payload.gameState.players.find(
+            (player) => player.id === socket.id
+          )?.cards;
+          if (cards) {
+            setCards(cards);
+          }
+          setGameState(payload.gameState);
+        }
+      }
+    });
+  }, [setCards, setErrors, setGameState, socket]);
+
+  useEffect(() => {
+    handleSocketEvents();
+    return () => {
+      if (socket) {
+        socket.off(SocketEvents.ServerEvent);
+      }
+    };
+  }, [handleSocketEvents, socket]);
+
+  return null;
 };
--- client/src/gameState/gameState.ts
+++ client/src/gameState/gameState.ts
@@ -1,14 +1,18 @@
 import { atom } from 'jotai';
+import { Card, GameSessionState } from '../sharedTypes';
 
 interface InitialState {
   sessionId: string;
   teamCodes: string[];
   teamCode: string;
 }
-export const gameStateAtom = atom<InitialState>({
+export const gameInitStateAtom = atom<InitialState>({
   sessionId: '',
   teamCodes: [],
   teamCode: ''
 });
 
 export const errorAtom = atom<string>('');
+
+export const gameStateAtom = atom<GameSessionState | null>(null);
+export const cardsAtom = atom<Card[]>([]);
