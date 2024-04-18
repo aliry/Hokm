0cc92fb79f5f6716263cfba240cfbddb82081ad3
Ali <aliry@live.com>
2024-04-17 22:51:19 -0700

    previous tricks popup

--- client/src/components/GameStateBoard.tsx
+++ client/src/components/GameStateBoard.tsx
@@ -3,39 +3,85 @@ import Paper from '@mui/material/Paper';
 import { Team1Color, Team2Color } from '../gameConfigs';
 import './GameStateBoard.css';
 import { useAtom } from 'jotai';
-import { appStateAtom, gameStateAtom } from '../gameState/gameState';
-import { FC } from 'react';
+import { appStateAtom, gameStateAtom, playersAtom } from '../gameState/gameState';
+import { FC, useState } from 'react';
 import Chip from '@mui/material/Chip';
+import { Trick } from '../sharedTypes';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
 
 interface TrickChipProps {
   index: number;
-  onclick: () => void;
+  trick: Trick;
 }
 interface ScoreContainerProps {
   teamName: string;
   bgColor: string;
   gameScore?: number;
-  roundScore?: number;
+  tricks?: Trick[];
 }
 
-const TrickChip: FC<TrickChipProps> = ({ index, onclick }) => {
-  return <Chip sx={{ m: 0.1 }} label={`${index + 1}`} onClick={onclick} />;
+const TrickChip: FC<TrickChipProps> = ({ index, trick }) => {
+  const [players] = useAtom(playersAtom);
+  const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <>
+      <Chip aria-describedby={id} sx={{ m: 0.1 }} label={`${index + 1}`} onClick={handleClick} />
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        {players && trick.items.map((item, index) => {
+          const player = players[item.playerIndex]
+          const cardName = `${item.card.suit}_${item.card.value}`;
+
+          return (
+            <Box key={index} sx={{ m: 2, display: "flex", flexDirection: "row", alignItems: "center", gap: 1 }} >
+              <Typography variant='h6'>
+                {player?.name}
+              </Typography>
+              <img
+                src={`/images/cards/${cardName}.svg`}
+                alt={`${item.card.suit}${item.card.value}`}
+                style={{ width: 50, height: 70 }}
+              />
+
+            </Box>
+          );
+        })}
+      </Popover>
+    </>
+  );
 }
 
 const ScoreContainer: FC<ScoreContainerProps> = ({
   teamName,
   bgColor,
   gameScore,
-  roundScore,
+  tricks,
 }) => {
   const previousTricks = [];
-  if (roundScore && roundScore > 0) {
-    for (let i = 0; i < roundScore; i++) {
-      const onclick = () => {
-        // TODO: Implement trick click to show cards played by each player in the trick
-        console.log(`Trick ${i + 1} clicked`);
-      }
-      previousTricks.push(<TrickChip key={i} index={i} onclick={onclick} />);
+  if (tricks && tricks.length > 0) {
+    for (let i = 0; i < tricks.length; i++) {
+      previousTricks.push(<TrickChip key={i} index={i} trick={tricks[i]} />);
     }
   }
 
@@ -62,19 +108,23 @@ export const GameStateBoard = () => {
 
   const team1code = appState.teamCodes[0];
   const team2code = appState.teamCodes[1];
+
+  const team1Tricks = gameState?.currentRound?.tricks.filter(trick => trick.winnerIndex === 0 || trick.winnerIndex === 2);
+  const team2Tricks = gameState?.currentRound?.tricks.filter(trick => trick.winnerIndex === 1 || trick.winnerIndex === 3);
+
   return (
     <Box className="state-board-container">
       <ScoreContainer
         teamName='Team 1'
         bgColor={Team1Color}
         gameScore={gameState?.scores[team1code]}
-        roundScore={gameState?.currentRound?.score[team1code]}
+        tricks={team1Tricks}
       />
       <ScoreContainer
         teamName='Team 2'
         bgColor={Team2Color}
         gameScore={gameState?.scores[team2code]}
-        roundScore={gameState?.currentRound?.score[team2code]}
+        tricks={team2Tricks}
       />
     </Box>
   );
